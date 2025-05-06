4cae2de9da1152551cb896ce8e65cc866d192cc1
m4jki90 <michalczerwinski123@gmail.com>
2025-05-06 11:57:07 +0200

    Add files via upload

--- US States Game/main.py
+++ US States Game/main.py
@@ -14,10 +14,7 @@ states = data.state.tolist()
 while score < 50:
     answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's the another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states:
-            if state not in guesses:
-                missing_states.append(state)
+        missing_states = [state for state in guesses if state not in guesses]
         missing = pandas.DataFrame(missing_states)
         missing.to_csv("learn.csv")
         break
