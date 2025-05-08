82e753fbdab4107c138add1659c0c35eb457b6dd
m4jki90 <michalczerwinski123@gmail.com>
2025-05-08 23:28:56 +0200

    Add files via upload

--- day26/NATO-alphabet-start/main.py
+++ day26/NATO-alphabet-start/main.py
@@ -1,6 +1,14 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 dict = {row.letter:row.code for (index,row) in data.iterrows()}
-word = input("Enter a word: ").upper()
-alphabet = [dict[letter] for letter in word]
-print(alphabet)
+def generate():
+    word = input("Enter a word: ").upper()
+    try:
+        alphabet = [dict[letter] for letter in word ]
+    except KeyError:
+        print("Sorry,only letters in the alphabet please.")
+        generate()
+    else:
+        print(alphabet)
+
+generate()
